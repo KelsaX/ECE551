57079264db38e7fa0729bb8c66e4826779091dc5
sx83 <sx83@duke.edu>
2023-10-03 16:31:27 -0400

    did step3

--- 043_eval1/sunspots.c
+++ 043_eval1/sunspots.c
@@ -89,9 +89,24 @@ void meanFilter(ss_monthly_t * data, size_t n, ss_monthly_t * mean, unsigned w) 
 
 double findLocalMax(ss_monthly_t * data, size_t n) {
   // WRITE ME
-  return 0;
+  //error case
+  if (n == 0) {
+    fprintf(stderr, "Invalid input!The input array has 0 elements!");
+    exit(EXIT_FAILURE);
+  }
+  //declare the largest index
+  size_t largestInd = 0;
+  //count i from 1 to n
+  for (size_t i = 1; i < n; i++) {
+    //compare every element with the current largest number
+    if (data[i].num > data[largestInd].num) {
+      largestInd = i;
+    }
+  }
+  //calculate the time stamp
+  double timeStamp = data[largestInd].year + data[largestInd].month / 12.0;
+  return timeStamp;
 }
-
 double calcSsPeriod(double * timeStamps, size_t n) {
   //WRITE ME
   return 0;
