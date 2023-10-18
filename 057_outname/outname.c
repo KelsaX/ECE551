b69f7ad8e8c6a946bdc6f1e9507f5060ce1c03c1
sx83 <sx83@duke.edu>
2023-10-18 08:36:33 -0400

    did057

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,15 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * suffix = ".counts";
+
+  int len = strlen(inputName) + strlen(suffix) + 1;
+  char * ans = malloc(len * sizeof(*ans));
+  snprintf(ans, len, "%s%s", inputName, suffix);
+  return ans;
 }
