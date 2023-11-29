d619c7929bee47ce9e6528c8ce7a134cfb4b01df
sx83 <sx83@duke.edu>
2023-11-28 22:59:55 -0500

    did079

---
+++ 079_sort_cpp/sortLines.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+//sort() is included in algorithm
+#include <algorithm>
+#include <fstream>
+#include <string>  //getline and string
+#include <vector>
+//vector用来存储读取到的string所以vector是一个string类型的
+
+int main(int argc, char ** argv) {
+  std::vector<std::string> lines;
+
+  if (argc == 1) {
+    std::string line;
+    while (std::getline(std::cin, line)) {
+      lines.push_back(line);
+    }
+    std::sort(lines.begin(), lines.end());
+    std::vector<std::string>::iterator it = lines.begin();
+    while (it != lines.end()) {
+      std::cout << *it << std::endl;
+      ++it;
+    }
+  }
+  else if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      std::string line;
+      std::ifstream file(argv[i]);
+      if (!file) {
+        std::cout << "could not open the file:" << argv[i] << std::endl;
+      }
+      while (std::getline(file, line)) {
+        lines.push_back(line);
+      }
+      std::sort(lines.begin(), lines.end());
+      std::vector<std::string>::iterator it = lines.begin();
+      while (it != lines.end()) {
+        std::cout << *it << std::endl;
+        ++it;
+      }
+
+      lines.clear();
+      file.close();
+    }
+  }
+
+  return (EXIT_SUCCESS);
+}
