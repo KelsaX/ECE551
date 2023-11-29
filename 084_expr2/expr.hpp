02d824aa7fb9f58ec76f5c6427f6b814c74092ed
sx83 <sx83@duke.edu>
2023-11-29 09:55:19 -0500

    did 084

---
+++ 084_expr2/expr.hpp
@@ -0,0 +1,97 @@
+#ifndef _EXPR_HPP_
+#define _EXPR_HPP_
+#include <sstream>
+#include <string>
+
+class Expression {
+ public:
+  Expression() {}
+  virtual std::string toString() const = 0;
+  virtual ~Expression() {}
+};
+
+class NumExpression : public Expression {
+ private:
+  long num;
+
+ public:
+  NumExpression(long num) : num(num) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << num;
+    return ans.str();
+  }
+  virtual ~NumExpression() {}
+};
+
+class PlusExpression : public Expression {
+ private:
+  Expression * lhs;
+  Expression * rhs;
+
+ public:
+  PlusExpression(Expression * lhs, Expression * rhs) : lhs(lhs), rhs(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << "(" << lhs->toString() << " + " << rhs->toString() << ")";
+    return ans.str();
+  }
+  ~PlusExpression() {
+    delete lhs;
+    delete rhs;
+  }
+};
+
+class MinusExpression : public Expression {
+ private:
+  Expression * lhs;
+  Expression * rhs;
+
+ public:
+  MinusExpression(Expression * lhs, Expression * rhs) : lhs(lhs), rhs(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << "(" << lhs->toString() << " - " << rhs->toString() << ")";
+    return ans.str();
+  }
+  ~MinusExpression() {
+    delete lhs;
+    delete rhs;
+  }
+};
+class TimesExpression : public Expression {
+ private:
+  Expression * lhs;
+  Expression * rhs;
+
+ public:
+  TimesExpression(Expression * lhs, Expression * rhs) : lhs(lhs), rhs(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << "(" << lhs->toString() << " * " << rhs->toString() << ")";
+    return ans.str();
+  }
+  ~TimesExpression() {
+    delete lhs;
+    delete rhs;
+  }
+};
+
+class DivExpression : public Expression {
+ private:
+  Expression * lhs;
+  Expression * rhs;
+
+ public:
+  DivExpression(Expression * lhs, Expression * rhs) : lhs(lhs), rhs(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream ans;
+    ans << "(" << lhs->toString() << " / " << rhs->toString() << ")";
+    return ans.str();
+  }
+  ~DivExpression() {
+    delete lhs;
+    delete rhs;
+  }
+};
+#endif
